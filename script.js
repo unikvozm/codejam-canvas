23f6ab01802b27c59b056bdd7d0e160389274512
Natalia Volkova <natalia.volkofff@gmail.com>
2019-11-09 11:12:02 +0300

    refactor: change readability of the code

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-var options = document.querySelectorAll(".switcher__option");
-var squares = document.querySelectorAll(".switcher__option-square");
+const options = document.querySelectorAll(".switcher__option");
+const squares = document.querySelectorAll(".switcher__option-square");
+const canvas = document.querySelector(".canvas");
+
+const activeSize = 'active-size';
+const activeSquare = 'active-square';
 
 options[0].addEventListener("click", function() {
-  options[0].classList.add("active-div");
-  options[1].classList.remove("active-div");
-  options[2].classList.remove("active-div");
-  squares[0].classList.add("active-square");
-  squares[1].classList.remove("active-square");
-  squares[2].classList.remove("active-square");
+  options[0].classList.add(activeSize);
+  options[1].classList.remove(activeSize);
+  options[2].classList.remove(activeSize);
+  squares[0].classList.add(activeSquare);
+  squares[1].classList.remove(activeSquare);
+  squares[2].classList.remove(activeSquare);
   drawCanvas(4);
 });
 
 options[1].addEventListener("click", function() {
-  options[1].classList.add("active-div");
-  options[0].classList.remove("active-div");
-  options[2].classList.remove("active-div");
-  squares[1].classList.add("active-square");
-  squares[0].classList.remove("active-square");
-  squares[2].classList.remove("active-square");
+  options[1].classList.add(activeSize);
+  options[0].classList.remove(activeSize);
+  options[2].classList.remove(activeSize);
+  squares[1].classList.add(activeSquare);
+  squares[0].classList.remove(activeSquare);
+  squares[2].classList.remove(activeSquare);
   drawCanvas(32);
 });
 
 options[2].addEventListener("click", function() {
-  options[2].classList.add("active-div");
-  options[0].classList.remove("active-div");
-  options[1].classList.remove("active-div");
-  squares[2].classList.add("active-square");
-  squares[0].classList.remove("active-square");
-  squares[1].classList.remove("active-square");
+  options[2].classList.add(activeSize);
+  options[0].classList.remove(activeSize);
+  options[1].classList.remove(activeSize);
+  squares[2].classList.add(activeSquare);
+  squares[0].classList.remove(activeSquare);
+  squares[1].classList.remove(activeSquare);
   drawCanvas(256);
 });
 
@@ -44,50 +48,61 @@ secondPic.forEach(row => {
   });
 });
 
+function activateFirstImage(ctx, cellWidth, cellHeight) {
+  firstPic.forEach((row, idxRow) => {
+    row.forEach((cell, idxCol) => {
+      ctx.fillStyle = "#" + cell;
+      ctx.fillRect(
+        cellWidth * idxCol,
+        cellHeight * idxRow,
+        cellWidth,
+        cellHeight
+      );
+    });
+  });
+}
+
+function activateSecondImage(ctx, cellWidth, cellHeight) {
+  secondPic.forEach((row, idxRow) => {
+    row.forEach((cell, idxCol) => {
+      ctx.fillStyle = `rgba(${cell})`;
+      ctx.fillRect(
+        cellWidth * idxCol,
+        cellHeight * idxRow,
+        cellWidth,
+        cellHeight
+      );
+    });
+  });
+}
+
+function activateThirdImage(ctx, width, height) {
+  let pic = new Image();
+  pic.onload = function() {
+    ctx.drawImage(pic, 0, 0, width, height);
+  };
+  pic.src = "./assets/image.png";
+}
+
 function drawCanvas(size) {
-  var canvas = document.querySelector(".canvas");
   canvas.width = 512;
   canvas.height = 512;
   canvas.style.backgroundColor = "white";
-  var ctx = canvas.getContext("2d"),
+  const ctx = canvas.getContext("2d"),
     cellWidth = Math.round(canvas.width / size),
     cellHeight = Math.round(canvas.height / size);
 
   switch (size) {
     case 4:
-      firstPic.forEach((row, idxRow) => {
-        row.forEach((cell, idxCol) => {
-          ctx.fillStyle = "#" + cell;
-          ctx.fillRect(
-            cellWidth * idxCol,
-            cellHeight * idxRow,
-            cellWidth,
-            cellHeight
-          );
-        });
-      });
+      activateFirstImage(ctx, cellWidth, cellHeight);
       break;
 
     case 32:
-      secondPic.forEach((row, idxRow) => {
-        row.forEach((cell, idxCol) => {
-          ctx.fillStyle = `rgba(${cell})`;
-          ctx.fillRect(
-            cellWidth * idxCol,
-            cellHeight * idxRow,
-            cellWidth,
-            cellHeight
-          );
-        });
-      });
+      activateSecondImage(ctx, cellWidth, cellHeight);
       break;
 
     case 256:
-      var pic = new Image();
-      pic.onload = function() {
-        ctx.drawImage(pic, 0, 0, canvas.width, canvas.height);
-      };
-      pic.src = "./assets/image.png";
+      activateThirdImage(ctx, canvas.width, canvas.height); 
       break;
   }
 }
